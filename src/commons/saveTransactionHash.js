beeec8c147e75836768ae2905e37a1cab57a7cd9
Ivan53471 <135232599+Ivan53471@users.noreply.github.com>
2024-09-20 20:01:54 +0800

    修复没有存TxHash与TokenURI关系

--- src/commons/saveTransactionHash.js
+++ src/commons/saveTransactionHash.js
@@ -1,18 +1,19 @@
 import axios from 'axios'; // 用于发送 HTTP 请求
 
-// 通过调用后端开放的接口
+// 调用后端接口存交易哈希
 async function saveTransactionHash(transactionHash, tokenURI) {
     try {
         // 通过交易哈希查询交易记录
-        const response = await axios.get('/api/getTxIDByTxHash', {
+        const apiBaseUrl = process.env.VUE_APP_BACKEND_BASE_URL;
+        const response = await axios.get(`${apiBaseUrl}/getTxIDByTxHash`, {
             params: { txHash: transactionHash }
         });
         
-        const transactionId = response.data.ID;
+        const transactionId = response.data.id;
 
         if (transactionId) {
             // 将 ID 和 tokenURI 存入数据库
-            await axios.post('/api/saveTxHash', {
+            await axios.post(`${apiBaseUrl}/saveTxHash`, {
                 txID: transactionId,
                 tokenURI: tokenURI,
             });
